3c134dd3812912c09148d3eb54305be738c726c5
CallMeKitsu <89200698+CallMeKitsu@users.noreply.github.com>
2022-11-04 14:09:32 +0100

    changed name

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Replit Box Remover
+// @name         BetterReplit by CMK.
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Customize and solve problems of Repl.it. By default, darker and cleaner than ever.
